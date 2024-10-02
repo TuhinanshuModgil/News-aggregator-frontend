23d78b0a6c183fb172f7ee411f00dfad7608850b
Tuhinanshu Modgil <tuhinanshumodgil18@gmail.com>
2024-10-02 13:30:21 +0530

    initial setup

---
+++ src/lib/carousel/card.svelte
@@ -0,0 +1,21 @@
+<script>
+    export let cardData = []
+</script>
+<div class="w-full h-full flex-shrink-0 p-4 overflow-auto">
+
+    <h3 class="mb-2">
+        {cardData.article_date}
+</h3>
+<h1 class="text-5xl font-semibold mb-2">
+    {cardData.article_title}
+</h1>
+<a href={cardData.article_url} target="_blank">
+
+    <div class="flex items-center mb-2 gap-2 text-blue-600">
+        <h5>Visit Source</h5> <span class="material-icons-outlined text-center text-blue-600">open_in_new</span> 
+    </div>
+</a>
+
+
+<p>{cardData.article_text}</p>
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,22 @@
+<script>
+	import Carousel from "../lib/carousel/carousel.svelte";
+    import data from "../lib/dummy_data/final_articles_techCrunch.json"
+    // let cardsData = [
+    //     {
+    //         name: "tuhi"
+    //     }
+    //     ,
+    //     {
+    //         name: "unnu"
+    //     }
+    // ]
+
+    let cardsData = data
+</script>
+
+<div class="w-screen h-screen "> 
+ <Carousel {cardsData}/>
+</div>
+
+
+  
